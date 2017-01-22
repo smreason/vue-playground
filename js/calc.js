8b93fd3f49293a3aa4d91906d954868903254c2a
Chris Quinn <cquinn@fool.com>
2017-01-22 00:56:06 -0500

    added calcs to handle how many times interest is compounded annualy

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,6 @@
+Vue.filter('currency', function (value) {
+	return "$" + value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+});
 
 var calculator = new Vue({
   el: '#calc',
@@ -19,6 +22,7 @@ var calculator = new Vue({
 		    amount: 10000,
 		    rate: 11.0,
 		    years: 10,
+		    timesCompound: 4,
 		    yearData: []
 		};
 	},
@@ -47,29 +51,45 @@ var calculator = new Vue({
   		return isValid;
   	},
   	computeReturns: function() {
-  		var year, yearGain, thisYear;
+  		var year, yearGain, period, periodGain, periodData;
   		var total = this.amount;
-
+  		var years = parseInt(this.years);
+  		var timesCompound = parseInt(this.timesCompound);
   		this.yearData = [];
 
-  		for (year=1; year <= this.years; year++) {
-  			yearGain = total * (this.rate/100);
-  			total += yearGain;
-  			thisYear = { 
-  				year: year,
-  				yearGain: yearGain,
-  				totalMoney: total,
-  				totalReturns: total - this.amount
-  			};
-			this.yearData.push(thisYear);
+  		for (year=1; year <= years; year++) {
+  			yearGain = 0;
+  			if (timesCompound === 0) {
+  				yearGain = this.amount * (this.rate/100);
+  				total += yearGain;
+  				periodData = { 
+	  				year: year,
+	  				yearGain: yearGain,
+	  				totalMoney: total,
+	  				totalReturns: total - this.amount
+	  			};
+	  			this.yearData.push(periodData);
+  			}
+  			else {
+  				for (period=1; period <= timesCompound; period++) {
+	  				periodGain = total * (this.rate/100)/timesCompound;
+	  				yearGain += periodGain;
+		  			total += periodGain;
+		  			periodData = { 
+		  				year: year,
+		  				yearGain: yearGain,
+		  				totalMoney: total,
+		  				totalReturns: total - this.amount
+		  			};
+		  			if (period === timesCompound) {
+		  				this.yearData.push(periodData);
+		  			}
+	  			}
+  			}
 		}
-  	}
+	}
   }
 });
 
-
-Vue.filter('currency', function (value) {
-	return "$" + value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-});
 
 
